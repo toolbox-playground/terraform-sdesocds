76cc04a2d2c414e2768e115d85d7de0558cb0a70
LeviRamosJr <levijrfjv@gmail.com>
2025-02-27 20:56:48 -0300

    Adicao do main.tf (#31)
    * Adicao do main.tf
    
    * correcao modulo
    
    * correcao

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ module "ec2_instances" {
   instance_names = { for i in range(1, 2) : "ec2-${i}" => "ec2-${i}" }
 }
 
+module "redes" {
+  source = "./modulos/rede"
+
+  vpc_cidr              = "10.0.0.0/16"
+  subnet_public_cidr_1  = "10.0.1.0/24"
+  subnet_public_cidr_2  = "10.0.2.0/24"
+  subnet_private_cidr_1 = "10.0.3.0/24"
+  subnet_private_cidr_2 = "10.0.4.0/24"
+  subnet_az1            = "us-west-2"
+  subnet_az2            = "us-west-2c"
+}
